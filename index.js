49b5873b097c38c9289fea875e2a191b4f71e24e
Nativo_00 <ornelasisac13@gmail.com>
2026-03-06 22:44:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -152,7 +152,7 @@ const helpCommand = {
 // === EVENTO: BOT PRONTO ===
 client.once('clientReady', async () => {
   console.log('\n' + chalk.green.underline('═'.repeat(50)));
-  console.log(chalk.green('   BOT ESTÁ ONLINE!'));
+  console.log(chalk.green('  ✅️ BOT ESTÁ ONLINE!'));
   console.log(chalk.green.underline('═'.repeat(50)));
 
   console.log(chalk.cyan('\n  📊 ESTATÍSTICAS INICIAIS:'));
